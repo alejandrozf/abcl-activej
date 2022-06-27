41e49a63112ad6e6df1750a48402c15060e4fb71
Alejandro Zamora Fonseca <ale2014.zamora@gmail.com>
2022-06-27 08:32:44 -0300

    added keep-alive parameter to even-loop creation

--- abcl-activej.lisp
+++ abcl-activej.lisp
@@ -4,10 +4,13 @@
 
 (let* ((el-create-method (java:jmethod "io.activej.eventloop.Eventloop" "create"))
        (el-post-method (java:jmethod "io.activej.eventloop.Eventloop" "post" "java.lang.Runnable"))
-       (el-run-method (java:jmethod "io.activej.eventloop.Eventloop" "run")))
+       (el-run-method (java:jmethod "io.activej.eventloop.Eventloop" "run"))
+       (el-keep-alive-method (java:jmethod "io.activej.eventloop.Eventloop" "keepAlive" "boolean"))
 
-  (defun make-event-loop ()
-    (java:jstatic el-create-method "io.activej.eventloop.Eventloop"))
+  (defun make-event-loop (&optional (keep-alive nil))
+    (let ((event-loop (java:jstatic el-create-method "io.activej.eventloop.Eventloop")))
+      (java:jcall el-keep-alive-method event-loop keep-alive)
+      event-loop))
 
   (defun post-to-event-loop (event-loop func)
     (let ((job (java:jinterface-implementation "java.lang.Runnable"
@@ -15,4 +18,9 @@
       (java:jcall el-post-method event-loop job)))
 
   (defun start-event-loop (event-loop)
-    (java:jcall el-run-method event-loop)))
+    (java:jcall el-run-method event-loop))
+
+  (defun stop-event-loop (event-loop)
+    (java:jcall el-run-method event-loop))
+
+  )
